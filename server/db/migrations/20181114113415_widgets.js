3065f7d157ccfc1bf7e888a518a9fda5a6666eaf
emily <emilycoco@me.com>
2019-03-04 15:37:44 +1300

    add rating field

--- server/db/migrations/20181114113415_widgets.js
+++ server/db/migrations/20181114113415_widgets.js
@@ -1,12 +1,13 @@
 exports.up = (knex, Promise) => {
-      return knex.schema.createTable('widgets', (table) => {
-        table.increments('id').primary()
-        table.string('name')
-        table.integer('price')
-        table.string('mfg')
-        table.integer('inStock')
-      })
-    }
+  return knex.schema.createTable('widgets', (table) => {
+    table.increments('id').primary()
+    table.string('name')
+    table.integer('price')
+    table.string('mfg')
+    table.integer('inStock')
+    table.integer('rating')
+  })
+}
 
 exports.down = (knex, Promise) => {
   return knex.schema.dropTable('widgets')
