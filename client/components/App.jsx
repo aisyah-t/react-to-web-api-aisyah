0abd5414c8f7a847bc2963ef93293834027b23ce
Derek <levidvi@gmail.com>
2020-06-23 10:38:26 +1200

    started work on app

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
-
-const App = () => {
-    return (
-      <div>
-        <h1>Widgets FTW!</h1>
-      </div>
-    )
+import { getWidgets } from "../api"
+class App extends React.Component {
+  state = {
+    Widgets: []
+  }
 }
+
+componentDidMount() {
+  console.log('mounted')
+  getWidgets().then(data => {
+    console.log('got widgets')
+    this.ListeningStateChangedEvent({
+      Widgets: data
+    })
+  })
+
+}
+render() {
+  console.log(this.state.Widgets)
+  return (
+    <div>
+      <h1> I love beans</h1>
+    </div>
+  )
+}
+
+
 
 export default App
