64c60602bc5181dec96468c701664c69cad7c9ed
rubin-gower <rubinsemails@gmail.com>
2020-06-23 10:39:38 +1200

    staring chnges

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import { getWidgets } from "../api"
+class App extends React.Component {
+  state = {
+    Widgets: []
+  }
 
-const App = () => {
+  componentDidMount() {
+    console.log("mounted")
+    getWidgets().then(data => {
+      console.log("got Widgets")
+      this.setState({
+        Widgets: data
+      })
+    })
+
+  }
+  render() {
+    console.log(this.state.Widgets)
+    this.state.Widgets.map(elem => {console.log(elem)})
+    console.log("renderd")
     return (
       <div>
         <h1>Widgets FTW!</h1>
       </div>
     )
+  }
 }
 
 export default App
