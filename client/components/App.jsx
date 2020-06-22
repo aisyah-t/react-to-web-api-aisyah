bda93ea5a701737779c9059310d82a96331a87c2
student <student@eda-bc-02.localdomain>
2020-06-23 10:16:29 +1200

    HAve componentDidMount call getWidgets

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { getWidgets } from '../api.js'
 
 class App extends React.Component {
+  state = {
+    widgets: []
+  }
+
+  componentDidMount() {
+    console.log("did a mounty")
+    getWidgets()
+      .then(() => {console.log("it a then")})
+  }
+
   render() {
+    console.log("did a rendery")
     return (
       <div>
         <h1>Wiaadgets FTW!</h1>
