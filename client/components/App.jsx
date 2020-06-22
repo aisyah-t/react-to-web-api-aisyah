98e0d3aeb314ad784e11159497efa116668d4f6f
Ben Irvine <benji72027@gmail.com>
2020-06-23 10:44:19 +1200

    MVP complete

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
+import Api, { getWidgets } from '../api'
+import Widget from './Widget'
 
-const App = () => {
+class App extends React.Component {
+  state = {
+    widgets: []
+  }
+
+  componentDidMount() {
+    console.log('did mount')
+    getWidgets()
+      .then((widgetData) => {
+        console.log('Widgets done been got!')
+        this.setState({
+          widgets: widgetData
+        })
+      })
+  }
+  render() {
+    console.log('render')
     return (
       <div>
         <h1>Widgets FTW!</h1>
+        <ul>
+          {this.state.widgets.map((element) => {
+            return <Widget name={element.name} price={element.price} mfg={element.mfg} inStock={element.inStock} />
+          })}
+        </ul>
       </div>
     )
+  }
 }
 
 export default App
