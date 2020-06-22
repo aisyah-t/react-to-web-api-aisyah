a4b2effc31512e1973a86e41b675c84b90717908
Richard E <46254658+richard-eman@users.noreply.github.com>
2020-06-23 11:46:11 +1200

    trying to render stuff from array using map

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,11 +1,48 @@
 import React from 'react'
+import { getWidgets } from '../api'
+import Widget from './Widget'
 
-const App = () => {
+class App extends React.Component {
+  
+  state = {
+    widgets : []
+  }
+  
+  componentDidMount(){
+    console.log('did mount')
+    console.log('render')
+    getWidgets()
+      .then((data) => {
+        console.log("getWidgets return:", data)
+        this.setState({
+          widgets: data
+        })
+        console.log("value of property widgets set:", this.state.widgets)
+      })
+  }
+
+  // make widget component that will get mapped onto the page
+    // widget comp. returns <li>
+    // .map widget into <ul>
+      // use data from state somehow
+        // can map put one object into one <li>?
+        // map will run aslong as there are objects in the array. So each widget <li> will get an object.
+      // put data from each object into prop using map
+  render() {
     return (
       <div>
         <h1>Widgets FTW!</h1>
+        <ul>
+          <Widget
+          widgetObject={
+            this.state.widgets.map(eachObject => {
+              return eachObject
+            })
+            } />
+        </ul>
       </div>
     )
+  }
 }
 
 export default App
---
+++ client/components/Widget.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Widget = props => {
+    console.log("hmm", props.widgetObject)
+    return(
+        <li>yo</li>
+    )
+}
+
+export default Widget
