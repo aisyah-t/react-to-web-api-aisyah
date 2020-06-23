3beca2a4a9f8dab16ce7651ebe9cebb4211e3277
Richard E <46254658+richard-eman@users.noreply.github.com>
2020-06-23 15:25:56 +1200

    Remove most console logs

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -13,21 +13,12 @@ class App extends React.Component {
     console.log('render')
     getWidgets()
       .then((data) => {
-        console.log("getWidgets return:", data)
         this.setState({
           widgets: data
         })
-        console.log("value of property widgets set:", this.state.widgets)
       })
   }
 
-  // make widget component that will get mapped onto the page
-    // widget comp. returns <li>
-    // .map widget into <ul>
-      // use data from state somehow
-        // can map put one object into one <li>?
-        // map will run aslong as there are objects in the array. So each widget <li> will get an object.
-      // put data from each object into prop using map
   render() {
     return (
       <div>
--- client/components/Widget.jsx
+++ client/components/Widget.jsx
@@ -1,7 +1,6 @@
 import React from "react"
 
 const Widget = props => {
-    console.log("Is this working? Object passed through props:", props.widgetObject)
     return(
     <li>{props.widgetObject.name} {props.widgetObject.price} {props.widgetObject.mfg}</li>
     )
