352a0a6ccd25288b267ee9d4ecc8e60a3a034ddd
fai-songprasit <prapawan.songprasit@gmail.com>
2020-06-25 11:21:34 +1200

    attampt show/hide edit menu

--- client/components/Update.jsx
+++ client/components/Update.jsx
@@ -5,7 +5,9 @@ import { updateWidget } from '../api'
 
 //same path pattern as Delete.jsx
 class Update extends React.Component {
-    state = {}
+    state = {
+        clicked: false
+    }
 
     handleSubmit = event => {
         event.preventDefault()
@@ -22,8 +24,19 @@ class Update extends React.Component {
         })
     }
 
+    editClick = () => {
+        if (this.state.clicked == false) {
+            this.setState({
+                [this.state.clicked]: true
+            })
+        } else if (this.state.clicked == true) {
+            this.setState({
+                [this.state.clicked]: false
+            })
+        }
+    }
+
     //alternative to this method would be to set input value with default value so that the form is already prefilled and no blanks are being pushed
-    
     //triggered wheneever props changed on component
     // static getDerivedStateFromProps(nextProps, prevState) {
     //     console.log(nextProps, prevState)
@@ -36,29 +49,33 @@ class Update extends React.Component {
     // }
 
     render() {
-        return (
-            <div class="update">
-            <h3>Edit this Widget</h3>
-                <form onSubmit={this.handleSubmit}>
-                    <label>Name:</label>
-                    <input type="text" name="name" value={this.state.name} defaultValue={this.props.widget.name} onChange={this.handleChange}/>
+        if (this.state.clicked == false) {
+            return <button onClick={this.editClick}>Edit</button>
+        } else if (this.state.clicked == true) {
+            return (
+                <div class="update">
+                <h3>Edit this Widget</h3>
+                    <form onSubmit={this.handleSubmit}>
+                        <label>Name:</label>
+                        <input type="text" name="name" value={this.state.name} defaultValue={this.props.widget.name} onChange={this.handleChange}/>
 
-                    <label>Price:</label>
-                    <input type="text" name="price" value={this.state.price} onChange={this.handleChange}/>
+                        <label>Price:</label>
+                        <input type="text" name="price" value={this.state.price} defaultValue={this.props.widget.price} onChange={this.handleChange}/>
 
-                    <label>MFG:</label>
-                    <input type="text" name="mfg" value={this.state.mfg} onChange={this.handleChange}/>
+                        <label>MFG:</label>
+                        <input type="text" name="mfg" value={this.state.mfg} defaultValue={this.props.widget.mfg} onChange={this.handleChange}/>
 
-                    <label>Stock:</label>
-                    <input type="text" name="inStock" value={this.state.inStock} onChange={this.handleChange}/>
+                        <label>Stock:</label>
+                        <input type="text" name="inStock" value={this.state.inStock} defaultValue={this.props.widget.inStock} onChange={this.handleChange}/>
 
-                    <label>Rating:</label>
-                    <input type="text" name="rating" value={this.state.rating} onChange={this.handleChange}/>
+                        <label>Rating:</label>
+                        <input type="text" name="rating" value={this.state.rating} defaultValue={this.props.widget.rating} onChange={this.handleChange}/>
 
-                    <button type="submit">Edit</button>
-                </form>
-            </div>
-        )
+                        <button type="submit">Submit</button>
+                    </form>
+                </div>
+            )
+        }
     }
 }
 
